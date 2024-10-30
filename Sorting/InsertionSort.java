5658fe76b6c68af97f733d2bc2712810e0dfb06c
balajiselvam005 <135320781+balajiselvam005@users.noreply.github.com>
2024-10-31 02:03:28 +0530

    Update InsertionSort Java file

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class InsertionSort {
+    
+    private static void insertionSort(int[] arr, int n){
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number of elements in the array: ");
+            int n = sc.nextInt();
+            
+            int[] arr = new int[n];
+
+            System.out.println("Enter the elements of the array:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Array elements before sorting: ");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+
+            insertionSort(arr, n);
+
+            System.out.println("\nArray elements after sorting: ");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+    }
 }
